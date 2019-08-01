c15c28a47ed887b98b1bf5c8af2fd06c64d19033
mpetavy <mpetavy>
2019-08-01 21:41:45 +0200

    added sample scripts

---
+++ install.sh
@@ -0,0 +1,13 @@
+for dir in ./*/     # list directories in the form "/tmp/dirname/"
+do
+	dir=${dir%*/}      # remove the trailing "/"
+	dir=${dir##*/}
+	echo --------------------------------------------------------------------------
+	echo $dir
+	echo --------------------------------------------------------------------------
+	cd $dir
+	rm go.*
+	go get -u ./...
+	go install
+	cd ..
+done
---
+++ pull.sh
@@ -0,0 +1,11 @@
+for dir in ./*/     # list directories in the form "/tmp/dirname/"
+do
+	dir=${dir%*/}      # remove the trailing "/"
+	dir=${dir##*/}
+	echo --------------------------------------------------------------------------
+	echo $dir
+	echo --------------------------------------------------------------------------
+	cd $dir
+	git pull
+	cd ..
+done
