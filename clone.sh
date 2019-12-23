9b232b0682f5895752a2cb3c23406dfedfe0fe37
mpetavy <mpetavy>
2019-12-23 23:23:49 +0100

    Linux pipe usage droped with temp file

--- clone.sh
+++ clone.sh
@@ -1,1 +1,2 @@
-curl --user "mpetavy:$GITHUB_PWD" https://api.github.com/users/mpetavy/repos?per_page=1000 | jsonpath -q "$..[?(@.language == 'Go')].name" | while read -r line; do git clone https://github.com/mpetavy/$line; done
+curl --user "$GITHUB_USERNAME:$GITHUB_PASSWORD" https://api.github.com/users/mpetavy/repos?per_page=1000 > /tmp/repos.txt 
+jsonpath -f /tmp/repos.txt -q "$..[?(@.language == 'Go')].name" | while read -r line; do git clone https://github.com/mpetavy/$line; done
