f7d19eedf70387049de1cc2685c7cccfd2108df9
mpetavy <mpetavy>
2020-01-10 09:05:14 +0100

    use temp files instead of pipe

--- clone.bat
+++ clone.bat
@@ -1,7 +1,8 @@
 @echo off
 banner %0
 pause
-curl --user "%GITHUB_USERNAME%:%GITHUB_PWD%" https://api.github.com/users/mpetavy/repos?per_page=1000 | jsonpath -q "$..[?(@.private == false)].name" > %temp%\repos.txt
+curl --user "%GITHUB_USERNAME%:%GITHUB_PASSWORD%" "https://api.github.com/users/mpetavy/repos?per_page=1000" > %temp%\github.txt
+jsonpath -f %temp%\github.txt -q "$..[?(@.private == false)].name" > %temp%\repos.txt
 for /F "tokens=*" %%a in (%temp%\repos.txt) do (
     git clone https://github.com/mpetavy/%%a
 )
