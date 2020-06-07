3e8c1db37623a92e160756e2f678016c808a0cde
mpetavy <mpetavy>
2020-06-07 12:58:44 +0200

    common v1.1.31

--- clone.bat
+++ clone.bat
@@ -2,7 +2,7 @@
 banner %0
 pause
 curl --user "%GITHUB_USERNAME%:%GITHUB_PASSWORD%" "https://api.github.com/users/mpetavy/repos?per_page=1000" > %temp%\github.txt
-jsonpath -f %temp%\github.txt -q "$..[?(@.private == false)].name" > %temp%\repos.txt
+jsonpath -nb -f %temp%\github.txt -q "$..[?(@.private == false)].name" > %temp%\repos.txt
 for /F "tokens=*" %%a in (%temp%\repos.txt) do (
     git clone https://github.com/mpetavy/%%a
 )
