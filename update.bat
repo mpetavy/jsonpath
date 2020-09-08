bc56de019ab7a783aa59dbed079702f773c11305
mpetavy <mpetavy>
2020-09-08 12:34:53 +0200

    update.bat added

---
+++ update.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+banner %0
+pause
+go clean -cache
+for /d %%a in (*.) do (
+    echo ----------------------------------------------------------------------
+    echo %%a
+    echo.
+    cd %%a
+	go get github.com/mpetavy/common@v1.1.55
+    go mod tidy
+    go install
+    cd ..
+)
