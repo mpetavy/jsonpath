c819e04d51f518dfa0f212c6ad593cbf406ac82b
mpetavy <mpetavy>
2021-06-23 22:04:50 +0200

    common@v1.2.30 update

--- update.sh
+++ update.sh
@@ -5,7 +5,11 @@ do
 	echo --------------------------------------------------------------------------
 	echo $dir
 	cd $dir
-	go get github.com/mpetavy/common@v1.1.57
+	go get github.com/mpetavy/common@v1.2.30
+	# go get github.com/kardianos/service@8eb6809ed2e7911d9538c90cbba90fb58866285a
+	if [ -f "./update-vendor.sh" ]; then
+		sh ./update-vendor.sh
+	fi
 	go mod tidy
 	go install
 	cd ..
