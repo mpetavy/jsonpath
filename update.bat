cb7d2fdb160e3ae3d59aff101a4c6c0de58c573a
mpetavy <mpetavy>
2022-06-07 07:01:39 +0200

    GenerateRandom... -> Rnd...

--- update.bat
+++ update.bat
@@ -10,6 +10,9 @@ for /d %%a in (*.) do (
     cd %%a
     go get github.com/mpetavy/common@v1.4.1
     if exist update-vendor.bat call update-vendor.bat
+    if "%%a" == "hydra" (
+	go run . -codegen
+    )
     go mod tidy
     go install
     cd ..
