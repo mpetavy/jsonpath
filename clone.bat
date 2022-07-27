b0f68c1ea173a774f302b04dff5e0fcb2d87f9b7
mpetavy <mpetavy>
2022-07-27 17:36:07 +0200

    fixed @.owner.login='mpetavy'

--- clone.bat
+++ clone.bat
@@ -3,7 +3,7 @@ if #%GITHUB_USERNAME%# == ## goto username
 if #%GITHUB_PAT%# == ## goto pat
 banner %0
 curl --user "%GITHUB_USERNAME%:%GITHUB_PAT%" "https://api.github.com/users/mpetavy/repos?per_page=1000" > %temp%\github.txt
-jsonpath -nb -f %temp%\github.txt -q "$..[?(@.language=='Go')].name" > %temp%\repos.txt
+jsonpath -nb -f %temp%\github.txt -q "$..[?(@.owner.login=='mpetavy')].name" > %temp%\repos.txt
 for /F "tokens=*" %%a in (%temp%\repos.txt) do (
     git clone https://github.com/mpetavy/%%a
 )
--- clone.sh
+++ clone.sh
@@ -1,2 +1,2 @@
 curl --user "$GITHUB_USERNAME:$GITHUB_TOKEN" https://api.github.com/users/mpetavy/repos?per_page=1000 > /tmp/repos.txt 
-jsonpath -nb -f /tmp/repos.txt -q "$..[?(@.language=='Go')].name" | while read -r line; do git clone https://github.com/mpetavy/$line; done
+jsonpath -nb -f /tmp/repos.txt -q "$..[?(@.owner.login=='mpetavy')].name" | while read -r line; do git clone https://github.com/mpetavy/$line; done
