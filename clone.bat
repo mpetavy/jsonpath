5b69cecbd11b4927a45bffe42c0959fe814b543e
mpetavy <petavy@gmx.net>
2023-10-12 13:04:13 +0200

    clone pith private repos

--- clone.bat
+++ clone.bat
@@ -2,7 +2,8 @@
 if #%GITHUB_USERNAME%# == ## goto username
 if #%GITHUB_PAT%# == ## goto pat
 banner %0
-curl --user "%GITHUB_USERNAME%:%GITHUB_PAT%" "https://api.github.com/users/mpetavy/repos?per_page=1000" > %temp%\github.txt
+curl --user "%GITHUB_USERNAME%:%GITHUB_PAT%" "https://api.github.com/user/repos?per_page=1000" > %temp%\github.txt
+rem curl --user "%GITHUB_USERNAME%:%GITHUB_PAT%" "https://api.github.com/users/mpetavy/repos?per_page=1000" > %temp%\github.txt
 jsonpath -nb -f %temp%\github.txt -q "$..[?(@.owner.login=='mpetavy')].name" > %temp%\repos.txt
 for /F "tokens=*" %%a in (%temp%\repos.txt) do (
     git clone https://github.com/mpetavy/%%a
