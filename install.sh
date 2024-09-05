efd084724b207b344d405d27da43c27c842dc6d8
mpetavy <mpetavy>
2024-09-05 23:42:20 +0200

    common@v1.8.74 update

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-set -e
+# set -e
 
 for dir in ./*/     # list directories in the form "/tmp/dirname/"
 do
 	dir=${dir%*/}      # remove the trailing "/"
 	dir=${dir##*/}
+	if [ ! -f "$dir/go.mod" ]; then
+		continue
+	fi
 	echo --------------------------------------------------------------------------
 	echo $dir
 	cd $dir
